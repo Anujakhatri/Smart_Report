2cb08f10f20739884716268794e32daf5bb0f65d
Anuja khatri <khatrianuja20@gmail.com>
2026-06-06 18:12:20 +0545

    feat: initialize React frontend project with Vite and cleanup backend URL configuration

--- SmartReport/SmartReport/urls.py
+++ SmartReport/SmartReport/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from apps.reports.views import home
-from apps.accounts.views import register_page, login_page, profile_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('register/', register_page, name='register_page'),
-    path('login/', login_page, name='login_page'),
-    path('profile/', profile_page, name='profile_page'),
     path('api/auth/', include('apps.accounts.urls')),
     path('api/rbac/',include('apps.roles.urls')),
     path('api/reports/',include('apps.reports.urls')),
