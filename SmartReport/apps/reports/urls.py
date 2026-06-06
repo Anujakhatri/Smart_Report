e1cd72bed509ecb43c3f99a26d93188714668981
Anuja khatri <khatrianuja20@gmail.com>
2026-06-06 19:22:16 +0545

    feat: implement regions module and migrate authentication templates to core application

---
+++ SmartReport/apps/reports/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from apps.reports.views import ReportCreateView, ReportListView
+
+urlpatterns = [
+#     path('create/', ReportCreateView.as_view(), name='report-create'),
+#     path('list/', ReportListView.as_view(), name='report-list'),
+]
