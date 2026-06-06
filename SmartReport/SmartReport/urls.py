9139d5174dea5bd1684fb27fe573d3e00389fc8a
Anuja khatri <khatrianuja20@gmail.com>
2026-06-06 17:55:55 +0545

    feat: implement user authentication templates and routes for profile, home, login, and registration

--- SmartReport/SmartReport/urls.py
+++ SmartReport/SmartReport/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from apps.reports.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('register/', TemplateView.as_view(template_name='register.html'), name='register_page'),
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login_page'),
-    path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile_page'),
+
     path('api/auth/', include('apps.accounts.urls')),
     path('api/rbac/', include('apps.roles.urls')),
     path('api/reports/', include('apps.reports.urls')),
